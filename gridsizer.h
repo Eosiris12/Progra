1670e9d846743a68b51cc89e5b11d755e2e26113
Miguel <miguelon777@gmail.com>
2017-10-31 18:03:30 -0500

    para mi amor penagos

---
+++ gridsizer.h
@@ -0,0 +1,29 @@
+#include <wx/wx.h>
+#include <iostream>
+using namespace std;
+
+class GridSizer : public wxFrame
+{
+public:
+  GridSizer(const wxString& title);
+
+  wxBoxSizer *sizer;
+  wxGridSizer *gs;
+  wxTextCtrl *display;
+  
+  void onCls (wxCommandEvent&);
+  void onBt1 (wxCommandEvent&);
+  void onBt2 (wxCommandEvent&);
+  void onBtSum (wxCommandEvent&);
+  void onBtPrd (wxCommandEvent&);
+  void onBtRst (wxCommandEvent&);
+  void onBtDiv (wxCommandEvent&);
+
+private:
+  int valor1;
+  int valor2;
+  string operacion;
+  int nClicks;
+  	
+  DECLARE_EVENT_TABLE()
+};
